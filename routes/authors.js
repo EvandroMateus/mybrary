72d99ba577ed83dd87356dd24cce74967d38fdb0
Evandro Mateus <evandromateus06@gmail.com>
2021-02-12 16:22:39 -0300

    publicar livros

--- routes/authors.js
+++ routes/authors.js
@@ -10,7 +10,7 @@ router.get('/', async (req, res) => {
     try{
         const authors = await Author.find(searchOptions)
         res.render('authors/index', { authors, searchOptions: req.query })
-    }catch(error){
+    }catch{
         res.redirect('/')
     }
 })
